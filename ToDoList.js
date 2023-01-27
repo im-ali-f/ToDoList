60c8250c6367416b4e716b4b467acd7aec28af51
im-ali-f <alifarhad.asus@gmail.com>
2023-01-27 15:06:34 +0330

    add BTN added & menu fixed

--- ToDoList.js
+++ ToDoList.js
@@ -4,4 +4,9 @@ menuTimeScheduleBTN.addEventListener("click",(e)=>{
     menuTimeScheduleBTN.classList.toggle('menuTimeScheduleBTN_color');
     menuTimeSchedule.classList.toggle('menuTimeSchedule_display');
 })
-    
+const addBTN = document.querySelector(".addBTN")
+const add = document.querySelector(".add")
+addBTN.addEventListener("click",(e)=>{
+    addBTN.classList.toggle('addBTN_color');
+    add.classList.toggle('add_display');
+})    
